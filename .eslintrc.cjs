6ee982baaafe08d05a8cc4b2085ce001d79f4a01
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-14 03:05:57 -0300

    chore(codestyle): add prettier and eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import ";',
+      },
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@ast": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@cmp": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@utl": fileURLToPath(new URL("./src/utils", import.meta.url)),
+    },
+  },
+  server: {
+    port: 9000,
+  },
+});
