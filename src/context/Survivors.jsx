cc5907f146a2da6412444f646878fa260f6abe21
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-03 13:54:21 -0300

    feat(store): functions to filter by community and infected people

--- src/context/Survivors.jsx
+++ src/context/Survivors.jsx
@@ -7,39 +7,69 @@ import survivorsList from "@api/survivors.json"
 const Survivors = createContext()
 
 export const SurvivorsProvider = ({ children }) => {
-  const [search, setSearch] = useState("")
+  // data
   const [list, setList] = useState([])
+  const [communityList, setCommunityList] = useState([])
+
+  // filtering
   const [results, setResults] = useState([])
+  const [search, setSearch] = useState("")
+  const [community, setCommunity] = useState("")
+  const [infected, setInfected] = useState(false)
 
-  const filter = text => {
-    if (!text) setResults(list)
+  const filterName = people => {
+    if (!search) return people
 
-    const searchText = text.toLowerCase()
-    const filteredResults = list.filter(({ name }) =>
-      name.toLowerCase().includes(searchText)
-    )
+    const searchText = search.toLowerCase()
+    return people.filter(({ name }) => name.toLowerCase().includes(searchText))
+  }
+
+  const filterCommunity = people => {
+    if (!community) return people
+    return people.filter(person => person.community === community)
+  }
+
+  const filterInfected = people => {
+    if (!infected) return people
+    return people.filter(person => person.infected)
+  }
+
+  const filter = () => {
+    let filteredResults = filterInfected(list)
+    filteredResults = filterCommunity(filteredResults)
+    filteredResults = filterName(filteredResults)
+
     setResults(filteredResults)
   }
 
-  const updateSearch = text => {
-    setSearch(text)
-    filter(text)
-  }
+  useEffect(() => {
+    filter()
+  }, [search, community, infected])
 
   useEffect(() => {
     const survivors = getFromStorage("survivors") || survivorsList
+    saveToStorage(survivors)
+
     setList(survivors)
     setResults(survivors)
-    saveToStorage(survivors)
+    // removing duplicates and sorting
+    setCommunityList(
+      [...new Set(survivors.map(({ community }) => community))].sort()
+    )
   }, [])
 
   return (
     <Survivors.Provider
       value={{
         list,
+        communityList,
         results,
         search,
-        updateSearch,
+        setSearch,
+        community,
+        setCommunity,
+        infected,
+        setInfected,
       }}
     >
       {children}
