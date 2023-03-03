28c754c25d9ca6cde9aecb0a9a4a49450aed1920
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-03 02:21:52 -0300

    feat(components): show a list of survivors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { SurvivorsProvider } from "@ctx/Survivors"
+import Table from "@cmp/Table"
+
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <SurvivorsProvider>
+      <div className="App">
+        <Table />
+      </div>
+    </SurvivorsProvider>
+  )
 }
 
 export default App
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,37 @@
+import { useSurvivorsContext } from "@ctx/Survivors"
+
+import styles from "./styles.module.scss"
+
+const Table = () => {
+  const { results } = useSurvivorsContext()
+
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <th>birthday</th>
+            <th>phone</th>
+            <th>community</th>
+            <th>infected</th>
+          </tr>
+        </tbody>
+        <tbody>
+          {results.map(({ name, birthday, phone, community, infected }) => (
+            <tr key={name + birthday}>
+              <td>{name}</td>
+              <td>{birthday}</td>
+              <td>{phone}</td>
+              <td>{community}</td>
+              <td>
+                <input type="checkbox" checked={infected} readOnly />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default Table
