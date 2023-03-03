12431f747c8c9254dcf6b0faf01fcca66e7283be
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-03 03:05:57 -0300

    feat(styling): create container mixins and use it into table component

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: "",
+        additionalData: "@import '@ast/mixins.scss';",
       },
     },
   },
