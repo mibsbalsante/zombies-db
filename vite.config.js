5573d9e9622c05ca0de94b036a804f7d97faa4ee
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-03 15:40:38 -0300

    chore(dependencies): svgo lib to load svg files as react components

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,14 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: "1.5rem",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -19,6 +27,9 @@ export default defineConfig({
       "@ast": fileURLToPath(new URL("./src/assets", import.meta.url)),
       "@cmp": fileURLToPath(new URL("./src/components", import.meta.url)),
       "@ctx": fileURLToPath(new URL("./src/context", import.meta.url)),
+      "@fa": fileURLToPath(
+        new URL("./src/assets/fontawesome/solid", import.meta.url)
+      ),
       "@utl": fileURLToPath(new URL("./src/utils", import.meta.url)),
     },
   },
