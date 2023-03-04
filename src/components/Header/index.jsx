3d9b52c861eab510a074b6e14e8cef8ca1837985
Melissa Balsante <mibsbalsante@gmail.com>
2023-03-04 04:28:53 -0300

    feat(styling): add icon to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,11 @@
+import { ReactComponent as Icon } from "@fa/biohazard.svg"
+
 import styles from "./styles.module.scss"
 
 const Header = () => (
   <header className={styles.header}>
     <div className={styles.headerContainer}>
+      <Icon className={styles.headerIcon} />
       <h1 className={styles.headerTitle}>{"Survivor's List"}</h1>
     </div>
   </header>
